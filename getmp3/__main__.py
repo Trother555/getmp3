2859dc596cb1dd990528c7e63382ebc589a625bf
Pavel Zharov <trother555@gmail.com>
2021-03-27 23:54:01 +0300

    fix excessive path separator appending to --path

--- getmp3/__main__.py
+++ getmp3/__main__.py
@@ -20,11 +20,12 @@ def main():
     temp_path = yt.streams.get_highest_resolution().download(
         output_path=tempfile.gettempdir()
     )
-    out_name = ''
     if not args.path.endswith('mp3'):
         out_name = (os.path.splitext(os.path.basename(temp_path))[0] +
                     '.mp3')
-    out_path = os.path.join(args.path, out_name)
+        out_path = os.path.join(args.path, out_name)
+    else:
+        out_path = args.path
     print(f'Saving mp3 into {out_path}')
     ffmpeg.input(temp_path).output(out_path, loglevel='error').run()
     print(f'Removing temp file {temp_path}')
